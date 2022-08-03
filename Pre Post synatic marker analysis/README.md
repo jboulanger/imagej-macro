e3b6973e87f9ff6f0320fe253f5be041e569a3ae
jboulanger <jerome.boulanger@laposte.net>
2022-08-03 12:31:48 +0100

    add readme

---
+++ Pre Post synatic marker analysis/README.md
@@ -0,0 +1,6 @@
+# Pre and post synaptic marker cluster 
+
+The macro Spot3D_Distance localizes marker in channels of interest as blobs and locate the nearest neighbour in each channels. 
+
+In a second step, the macro Analyze_Spot3D_Distance finds clusters of multiple markers based on the proximity threshold.
+
--- Pre Post synatic marker analysis/make_graphs.r
+++ Pre Post synatic marker analysis/make_graphs.r
@@ -2,48 +2,38 @@ library(ggplot2)
 library(tidyverse)
 library(infer)
 
+folder = file.choose(new = FALSE)
+
 # load the information about the series extracted from the lif file
 # extract the condition by parsing the serie name
-info <- read_csv("/media/micro-nas-1/Greger_lab/imogen/202622/info.csv",show_col_types = FALSE) %>%
+info <- read_csv(file.path(folder, "info.csv"),show_col_types = FALSE) %>%
   mutate(Condition = ifelse(str_detect(`Serie name`,".*wt.*"),"WT","dPDZ"))  %>%
   rename(`Image Name`=`Output  file`)
 
-
+# reorder the groups
+info$Condition <- ordered(info$Condition, levels=c("WT","dPDZ"))
 
 # load the overlap table and make a join to get the conditions
-overlap <- read_csv("/media/micro-nas-1/Greger_lab/imogen/202622/result/overlaps.csv",show_col_types = FALSE) %>%
-  na_if("NaN") %>%
+overlap <- read_csv(file.path(folder, "result","overlaps.csv"),show_col_types = FALSE) %>%
   inner_join(info,by="Image Name")
 
 # number of chained cluster by condition
-overlap %>% ggplot(aes(x=`Condition`,y=`# chained`))+ geom_point()
+overlap %>% ggplot(aes(x=`Condition`,y=`# chained`)) + geom_boxplot()
 
 # ratio number of chained cluster vs number of bassoon
 overlap %>% ggplot(aes(x=`Condition`,y=`# chained`/`bassoon`))+ geom_boxplot()
 
-
+overlap %>% t_test(formula=`# chained`~`bassoon`)
 
 # load the chained clusters table and add the condition
-cl <- read_csv("/media/micro-nas-1/Greger_lab/imogen/202622/result/clusters.csv",show_col_types = FALSE)
+cl <- read_csv(file.path(folder, "result","clusters.csv"),show_col_types = FALSE)
 
-%>%
-
-
-  na_if("NaN") %>%
-  inner_join(info,by="Image Name")
-
-# count the number of clusters per image
+# count the number of clusters per image from the cluster.csv file
 cl %>%
   group_by(`Image Name`) %>%
-  filter(`Distance [glua2:bassoon]` < 0.25) %>%
   summarise(N = n()) %>%
-  inner_join(info,by='Image Name')
-
-%>%
-  ggplot(aes(x=`Condition`,y=`n`)) +
+  inner_join(info,by='Image Name') %>%
+  ggplot(aes(x=`Condition`,y=`N`)) +
   geom_boxplot() +
   ggtitle("Number of 3 colours clusters per image")
 
-cl %>%
-  ggplot(aes(x=`Condition`,y=`Distance [psd95:glua2]`)) +
-  geom_violin()
